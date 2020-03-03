097c507877b95ff8d2855aa620e9dcf8c07b6eb5
Test <Test@local.itstep>
2020-03-03 17:30:47 +0600

    input fix

--- main.py
+++ main.py
@@ -9,34 +9,33 @@ myQuestions = cursor.fetchall()
 finalPlayerClass = ''
 conn.close()
 playerAnswer = {'Combat' : 0,'Magic' : 0,'Stealth' : 0}
-i = 1
+
 for item in myQuestions:
     while True:
-        print(str(i) + ".Вопрос " + item[1] + "\n")
+        print(item[1] + "\n")
         print('1. ' + item[2] + "\n")
         print('2. ' + item[3] + "\n")
         print('3. ' + item[4] + "\n")
         tmpAnswer = input()
         print()
         if tmpAnswer == '1':
-            playerAnswer['Combat'] = playerAnswer['Combat'] + 1
+            playerAnswer['Combat'] += 1
             break;
         elif tmpAnswer == '2':
-            playerAnswer['Magic'] = playerAnswer['Combat'] + 1
+            playerAnswer['Magic'] += 1
             break;
         elif tmpAnswer == '3':
-            playerAnswer['Stealth'] = playerAnswer['Combat'] + 1
+            playerAnswer['Stealth'] += 1 
             break;
         else:
             print('error')
-    i= i + 1
 
-if playerAnswer['Combat'] >= 7:
+if playerAnswer['Combat'] > 6:
     playerAnswer['Combat'] = 7
-if playerAnswer['Magic'] >= 7:
+if playerAnswer['Magic'] > 6:
     playerAnswer['Magic'] = 7
-if playerAnswer['Stealth'] >= 7:
-    playerAnswer['Stealth']
+if playerAnswer['Stealth'] > 6:
+    playerAnswer['Stealth'] = 7
 
 
 for item in myClass:
@@ -46,12 +45,13 @@ for item in myClass:
         finalPlayerClass = item[1]
     if playerAnswer['Stealth'] == 7 and item[4] == '7':
         finalPlayerClass = item[1]
-    if playerAnswer['Stealth'] == 4 and item[4] == '0-6':
+    if playerAnswer['Combat'] == 4 and item[4] == '0-6':
         finalPlayerClass = item[1]
     
     try:
-        if playerAnswer['Combat'] == int(item[2]) and playerAnswer['Magic'] == int(item[3]) and playerAnswer['Stealth'] == int(item[4]):
+        if str(playerAnswer['Combat']) == item[2] and str(playerAnswer['Magic']) == item[3] and str(playerAnswer['Stealth']) == item[4]:
             finalPlayerClass = item[1]
     except:
         print()
-print(finalPlayerClass)
+print('Answer:' + finalPlayerClass)
+print('Combat: ' + str(playerAnswer['Combat']) + ' Magic: ' + str(playerAnswer['Magic']) + ' Stealth: ' + str(playerAnswer['Stealth']))
